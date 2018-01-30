3c2705ee1036d7a8b6deebdb5f218d3c132ddedb
ys1125.lee <ys1125.lee@samsung.com>
2018-01-30 19:30:08 +0900

    add image
    image1

--- src/DashboardCard/DashboardCardHeader.js
+++ src/DashboardCard/DashboardCardHeader.js
@@ -19,7 +19,9 @@ class DashboardCardHeader extends React.Component {
     return (
       <div id="parent">
       <div className="CircleImage">
-          <Paper style={circleStyle} zDepth={1} circle={true} />
+          <Paper style={circleStyle} zDepth={1} circle={true} >
+          <img className="img-circle" src="http://img.tenasia.hankyung.com/webwp_kr/wp-content/uploads/2017/09/2017092116364588197-540x810.jpg" width="100%" height="100%" />
+          </Paper>
       </div>
       <div className="TopContainer"></div>
        <div className="CardHeader">
