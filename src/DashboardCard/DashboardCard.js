7b1085a2af0749e2b2ae10631988612570b80a8d
ys1125.lee <ys1125.lee@samsung.com>
2018-01-31 18:48:37 +0900

    props summary
    props summary for react dashboard

--- src/DashboardCard/DashboardCard.js
+++ src/DashboardCard/DashboardCard.js
@@ -16,36 +16,15 @@ const style = {
 class DashboardCard extends React.Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <Paper style={style} zDepth={3}>
-          <DashboardCardHeader imageSrc="http://img.tenasia.hankyung.com/webwp_kr/wp-content/uploads/2017/09/2017092116364588197-540x810.jpg" />
-          <DashboardCardTitle cardTitle="슬기" cardSubTitle="레드벨벳" />
+          <DashboardCardHeader imageSrc={this.props.imageSrc} />
+          <DashboardCardTitle cardTitle={this.props.cardTitle} cardSubTitle={this.props.cardSubTitle} />
           <DashboardCardContent>
-          dfsfs
+          {this.props.children}
           </DashboardCardContent>
         </Paper>
-        <Paper style={style} zDepth={3}>
-        <DashboardCardHeader imageSrc="http://image.fmkorea.com/files/attach/new/20171107/425547776/654145381/829082810/6d7fb5d52834d8a923c07dbe83822710.jpg" />
-        <DashboardCardTitle cardTitle="피카츄" cardSubTitle="전기 포켓몬" />
-        <DashboardCardContent>
-        dfsfs
-        </DashboardCardContent>
-        </Paper>
-        <Paper style={style} zDepth={3}>
-        <DashboardCardHeader imageSrc="http://img.danawa.com/images/descFiles/4/493/3492689_1511047416808.jpeg"/>
-        <DashboardCardTitle cardTitle="고양이" />
-        <DashboardCardContent>
-        dfsfs
-        </DashboardCardContent>
-        </Paper>
-        <Paper style={style} zDepth={3}>
-        <DashboardCardHeader imageSrc="http://img1.daumcdn.net/thumb/R1024x0/?fname=http://i1.daumcdn.net/cfile299/image/24599A4458B9A00516F789"/>
-        <DashboardCardTitle cardTitle="예린" cardSubTitle="여자친구" />
-        <DashboardCardContent>
-        dfsfs
-        </DashboardCardContent>
-        </Paper>
-      </div>
+      </React.Fragment>
       );
     }
 }
--- src/DashboardPage.js
+++ src/DashboardPage.js
@@ -5,7 +5,18 @@ class DashboardPage extends React.Component {
   render() {
     return (
       <div className="DashboardPage">
-        <DashboardCard />
+        <DashboardCard  imageSrc="http://img.tenasia.hankyung.com/webwp_kr/wp-content/uploads/2017/09/2017092116364588197-540x810.jpg" cardTitle="슬기" cardSubTitle="레드벨벳">
+        dfsfs
+        </DashboardCard>
+        <DashboardCard  imageSrc="http://image.fmkorea.com/files/attach/new/20171107/425547776/654145381/829082810/6d7fb5d52834d8a923c07dbe83822710.jpg" cardTitle="피카츄" cardSubTitle="전기 포켓몬">
+        dfsfs
+        </DashboardCard>
+        <DashboardCard  imageSrc="http://img.danawa.com/images/descFiles/4/493/3492689_1511047416808.jpeg" cardTitle="고양이">
+        dfsfs
+        </DashboardCard>
+        <DashboardCard  imageSrc="http://img1.daumcdn.net/thumb/R1024x0/?fname=http://i1.daumcdn.net/cfile299/image/24599A4458B9A00516F789" cardTitle="예린" cardSubTitle="여자친구">
+        dfsfs
+        </DashboardCard>
       </div>
     );
   }
